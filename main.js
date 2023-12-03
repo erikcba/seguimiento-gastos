37f64b032a2572d39fd0279f1e8da767b78bb1d0
Erik Tomaselli <eriktomaselli97@gmail.com>
2023-12-03 10:53:51 -0300

    agregado de verificacion en input

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const ctx = document.getElementById('myChart')
 import data from './data.mjs';
 
+
 let datosAlmacenados = JSON.parse(localStorage.getItem('datos') || data);
- 
+
 const today = (new Date(new Date().toLocaleString("en-US", { timeZone: "America/Argentina/Buenos_Aires" })).getDay() + 6) % 7;
 
 const defaultColor = 'hsl(10, 79%, 65%)';
@@ -50,17 +51,31 @@ function agregarGasto(event) {
   event.preventDefault()
   const gastoNumero = parseFloat(document.getElementById('gastoNumero').value)
 
-  datosAlmacenados[today].amount += gastoNumero;
+  if (gastoNumero > 0) {
+    datosAlmacenados[today].amount += gastoNumero;
 
-  actualizarChart()
-  
-  localStorage.setItem('datos', JSON.stringify(datosAlmacenados))
+    actualizarChart()
 
-  alert('Gasto agregado')
-  document.getElementById('gastoNumero').value = '';
+    localStorage.setItem('datos', JSON.stringify(datosAlmacenados))
 
-  total = datosAlmacenados.reduce((acc, cur) => acc + cur.amount, 0);
-  totalGasto.innerHTML = `$ ${total}`;
+    alert('Gasto agregado')
+    document.getElementById('gastoNumero').value = '';
+
+    total = datosAlmacenados.reduce((acc, cur) => acc + cur.amount, 0);
+    totalGasto.innerHTML = `$ ${total}`;
+  } else {
+    Swal.fire({
+      customClass: {
+        confirmButton: 'swalBtnColor'
+      },
+      title: 'Error!',
+      text: 'Debes agregar un gasto',
+      icon: 'error',
+      confirmButtonText: 'Entendido'
+    })
+  }
+
+
 }
 
 
@@ -68,7 +83,6 @@ function actualizarChart() {
   myChart.data.datasets[0].data = datosAlmacenados.map(row => row.amount);
   myChart.data.datasets[0].backgroundColor = datosAlmacenados.map((row, index) => index === today ? currentDayColor : defaultColor);
   myChart.update();
-  console.log(datosAlmacenados)
 }
 
 btnAgregar.addEventListener('click', agregarGasto)
